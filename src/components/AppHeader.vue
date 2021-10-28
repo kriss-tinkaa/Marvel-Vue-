6b5df3e2e24777f080ad4bd0e3eaf168d3c0465b
kriss-tinkaa <44407472+kriss-tinkaa@users.noreply.github.com>
2021-10-28 17:02:23 +0300

    Add Vue components: header, modal, main card

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+     <header>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand">Marvel</a>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search">
+            <button class="btn btn-light" type="submit">Все</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
